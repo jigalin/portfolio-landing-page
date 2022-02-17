936252ee3334ee8eb043e91af1ef15d7c7c82395
iana-sha <iana@zelexio.com>
2022-02-16 19:34:02 -0500

    skills api fixed

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,61 +1,83 @@
 import React, { useRef } from 'react'
+import axios from 'axios'
 import IsVisible from 'react-is-visible'
 import { Fade } from 'react-reveal'
 import { skills } from '../../data/skills.json'
 import { useContainerDimensions } from '../../hooks'
 
-const Skills = () => {
-  const skillsWrapper = useRef()
-  const { width } = useContainerDimensions(skillsWrapper)
 
-  return (
-    <Fade duration={1000}>
-      <div style={{ position: 'relative', width: '100%', maxWidth: 600 }}>
-        <IsVisible once>
-          {(isVisibleSkillsWrapper) => (
-            <div
-              className="skills-wrapper"
-              style={
-                isVisibleSkillsWrapper
-                  ? {
-                      transition: '1s opacity ease-in-out',
-                      transform: `translateX(0)`,
-                      opacity: 1,
-                    }
-                  : {}
-              }
-            >
-              <h2>Skills</h2>
-              <ul className="skills" ref={skillsWrapper}>
-                {skills.map((skills) => {
-                  return (
-                    <li className="skill-bar-wrapper" key={skills.skillName}>
-                      <div
-                        className="skill-bar"
-                        style={
-                          isVisibleSkillsWrapper
-                            ? {
-                                transition: `${
-                                  1 + skills.id / 10
-                                }s width ease-in-out`,
-                                width: width * (skills.amount / 100),
-                              }
-                            : {
-                                width: 1,
-                              }
-                        }
-                      ></div>
-                      <div className="skill-name">{skills.skillName}</div>
-                    </li>
-                  )
-                })}
+//const url = 'https://springboot-mysql-web-app.herokuapp.com/api/v1/skillsapi'
+
+//const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+class Skills extends React.Component {
+
+
+  // * constructor
+  // *
+  // * @object  @props  parent props
+  // * @object  @state  component state
+
+  constructor(props) {
+
+      super(props);
+
+      this.state = {
+          items: [],
+          isLoaded: false
+      }
+
+  }
+
+
+  // * componentDidMount
+  // *
+  // * Fetch json array of objects from given url and update state.
+
+  componentDidMount() {
+
+    axios.get('http://localhost:8080/api/v1/skillsapi', {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+          .then(res => {
+              this.setState({
+                  items: res.data,
+                  isLoaded: true, 
+              })
+          }).catch((err) => {
+              console.log(err);
+          });
+
+  }
+
+
+  // * render
+  // *
+   //* Render UI
+   
+  render() {
+
+      const { isLoaded, items } = this.state;
+
+      if (!isLoaded)
+          return <div>Loading...</div>;
+
+      return (
+          <div className="App">
+              <ul>
+                  {items.map(item => (
+                      <li key={item.id}>
+                          Skill Name: {item.skillName} | Amount: {item.amount}
+                      </li>
+                  ))}
               </ul>
-            </div>
-          )}
-        </IsVisible>
-      </div>
-    </Fade>
-  )
+          </div>
+      );
+
+  }
+
 }
 
-export default Skills
+export default Skills;
