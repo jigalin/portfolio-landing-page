cdf7673077c59682cc5a9566ffffb34e16999b37
iana-sha <iana@zelexio.com>
2022-02-16 19:34:42 -0500

    about page personalised to me

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,7 +14,7 @@ const About = () => {
           <div className="about-text">
             <h2>Who am I?</h2>
             <p>
-              I'm Matt{' '}
+              I'm Iana{' '}
               <span role="img" aria-label="lightning">
                 😄
               </span>{' '}
@@ -23,15 +23,13 @@ const About = () => {
               <span role="img" aria-label="lightning">
                 ⚡
               </span>{' '}
-              I'm a software engineer focusing on Front-End web and emerging
-              tech such as VR, AR and Blockchain.
+              I'm a software developper working with both Back-End and Front-End technologies.
             </p>
             <p>
               <span role="img" aria-label="lightning">
                 ⚡
               </span>{' '}
-              You can find me working with UE5, JavaScript, React, Vue, Node,
-              Apollo and Gatsby.
+              You can find me working with Java, C#, Python, JavaScript, React, Vue.
             </p>
             <div className="typewriter">
               <p className="typewriter-start">
@@ -46,7 +44,7 @@ const About = () => {
                     'learning new technologies',
                     'exercising',
                     'solving complex problems',
-                    'skiing',
+                    'sports',
                     'fixing hard-to-fix bugs',
                     'trying new things',
                     'collaborating with others',
@@ -58,7 +56,7 @@ const About = () => {
               <p>.</p>
             </div>
             <p>
-              Since 2015 - I've spent my time seeking and learning new
+              Since 2019 - I've spent my time seeking and learning new
               technologies and forms of digital expression. This has led to me
               working on some amazing world-class{' '}
               <Link
@@ -97,7 +95,7 @@ const About = () => {
                   d="M11.536 3.464a5 5 0 010 7.072L8 14.07l-3.536-3.535a5 5 0 117.072-7.072v.001zm1.06 8.132a6.5 6.5 0 10-9.192 0l3.535 3.536a1.5 1.5 0 002.122 0l3.535-3.536zM8 9a2 2 0 100-4 2 2 0 000 4z"
                 ></path>
               </svg>
-              <p>Sydney, Australia</p>
+              <p>Montreal, Canada</p>
             </div>
           </div>
         </Fade>
