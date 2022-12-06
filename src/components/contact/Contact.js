150ca7920e2883d54a9f35d72593ca1a63480dc9
Dileep Nimma <dileepreddyoff@gmail.com>
2022-12-05 19:12:39 -0500

    portfolio
    My portfolio

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,6 +6,7 @@ import ContactForm from '../contactForm/ContactForm'
 import linkedin from '../../images/social/linkedin.png'
 import github from '../../images/social/github.png'
 import facebook from '../../images/social/facebook.png'
+import discord from '../../images/social/discord.png'
 
 const Contact = () => {
   return (
@@ -14,22 +15,28 @@ const Contact = () => {
       <Bounce cascade>
         <div className="links">
           <a
-            href="https://github.com/jigalin"
+            href="https://github.com/NimmaDileep"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img src={github} alt="Github Logo" width="40px" />
           </a>
           <a
-            href="https://www.linkedin.com/in/mjigalin/"
+            href="https://www.linkedin.com/in/nimma-dileep-reddy/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img src={linkedin} alt="Linkedin Logo" width="40px" />
           </a>
-
           <a
-            href="https://www.facebook.com/matthew.jigalin/"
+            href="https://discord.com/users/AssassinWolf#2491"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={discord} alt="Facebook Logo" width="40px" />
+          </a>
+          <a
+            href="https://www.facebook.com/dileep.reddy.351"
             target="_blank"
             rel="noopener noreferrer"
           >
